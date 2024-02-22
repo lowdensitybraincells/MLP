d559611dab1feab002df27f259411b02a5bcbd73
labmday <lowdensitybraincells@gmail.com>
2024-02-22 21:50:26 +0100

    modified:   MLP.py 	modified:   layer.py

--- MLP.py
+++ MLP.py
@@ -1,13 +1,13 @@
 from dataReader import getData
 import matplotlib.pyplot as plt
-
+import numpy as np
+import layer
 
 if __name__ == "__main__":
-
     labels, images = getData()
-    
-    for i in range(9):
-        plt.subplot(330+1+i)
-        plt.imshow(images[i,:,:], cmap='gray')
-    plt.show()
+    L = layer.layer(2)
+    L.updateParams(np.array([[1,1],[1,2]]), np.array([1,-2]))
+    a = L.process([1, 1])
+    b = L.process([1, -1])
+    print(a, b)
     
---
+++ layer.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+
+def ReLU(x):
+    x = np.array(x)
+    return np.multiply(x, x>0, dtype=np.float32)
+
+class layer:
+    def __init__(self, size, activation = ReLU):
+        self.size = size
+        self.activation = activation
+        self.bias = np.zeros((size, 1))
+        self.weights = np.random.rand(size, size)
+
+    def process(self, x):
+        z = np.matmul(self.weights, x) + self.bias
+        a = self.activation(z)
+        return a 
+
+    def updateParams(self, newWeights, newBias):
+        self.weights = newWeights
+        self.bias = newBias 
